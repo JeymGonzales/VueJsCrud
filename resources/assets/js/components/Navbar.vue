e9c96de3ea9ff00334e44de7909fe5fe548325c7
Jem Gonzales <jeym.gonzales@gmail.com>
2018-07-20 12:48:07 +0800

    first commit

---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav class="navbar navar-expand-sm navbar-dark bg-info mb-2">
+        <div class="container">
+            <a href="" class="navbar-brand">
+                List
+            </a>
+        </div>
+    </nav>
+</template>
